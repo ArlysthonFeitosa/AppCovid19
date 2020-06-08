9eadf266542bc7ea71bc99322787586369e16ca8
Arlysthon Feitosa Freitas <arlysff04@gmail.com>
2020-06-08 17:18:35 -0300

    Classes juntas para otimizar organização

--- app/src/main/java/com/example/covid_19/MainActivity.java
+++ app/src/main/java/com/example/covid_19/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.covid_19;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -63,5 +71,93 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+}
+
+
+
+class CardMenu {
+    ImageView imagemCardMenu;
+    TextView tituloCardMenu;
+    TextView descricaoCardMenu;
+
+}
+
+
+class DadosCardMenu {
+    private int imagemCardMenu;
+    private String tituloCardMenu;
+    private String descricaoCardMenu;
+
+    public DadosCardMenu(int imagemCardMenu, String tituloCardMenu, String descricaoCardMenu) {
+        this.imagemCardMenu = imagemCardMenu;
+        this.tituloCardMenu = tituloCardMenu;
+        this.descricaoCardMenu = descricaoCardMenu;
+    }
+
+    public int getImagemCardMenu() {
+        return imagemCardMenu;
+    }
+
+    public String getTituloCardMenu() {
+        return tituloCardMenu;
+    }
+
+    public String getDescricaoCard() {
+        return descricaoCardMenu;
+    }
+
+}
+
+class CardMenuAdapter extends ArrayAdapter {
+
+    public CardMenuAdapter(@NonNull Context context, int resource) {
+        super(context, resource);
+    }
+
+    @Override
+    public void add(@Nullable Object object) {
+        super.add(object);
+    }
+
+    @Override
+    public int getCount() {
+        return super.getCount();
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View viewCardMenu = convertView; //Layout do cartão
+        CardMenu cardMenu; //O que tem no cartão
+
+        if(convertView == null){
+            LayoutInflater inflater = (LayoutInflater) this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            //Pega o layout do modelo
+            viewCardMenu = inflater.inflate(R.layout.modelolista_menu, parent, false);
+
+            cardMenu = new CardMenu();
+            cardMenu.imagemCardMenu = (ImageView) viewCardMenu.findViewById(R.id.imagemCardMenu);
+            cardMenu.tituloCardMenu = (TextView) viewCardMenu.findViewById(R.id.tituloCardMenu);
+            cardMenu.descricaoCardMenu = (TextView) viewCardMenu.findViewById(R.id.descricaoCardMenu);
+
+
+            viewCardMenu.setTag(cardMenu);
+
+
+        }else{
+            cardMenu = (CardMenu) viewCardMenu.getTag();
+        }
+
+        DadosCardMenu dadosCardMenu;
+        dadosCardMenu = (DadosCardMenu) this.getItem(position);
+
+        cardMenu.imagemCardMenu.setImageResource(dadosCardMenu.getImagemCardMenu());
+        cardMenu.tituloCardMenu.setText(dadosCardMenu.getTituloCardMenu());
+        cardMenu.descricaoCardMenu.setText(dadosCardMenu.getDescricaoCard());
+
+
+        return viewCardMenu;
     }
 }
